8f4d8b7338d001b1608ed75d2a621221531a90fc
gauravghodinde <gauravghodinde@gmail.com>
2024-03-15 03:55:47 +0530

    added the navbar animation when scroll is more than 15 and made its bg blur

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,41 @@
-import React, { useEffect, useRef } from "react";
-import './Navbar.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./Navbar.css";
 const Navbar = () => {
   const navRef = useRef(null);
+  const [show, setShow] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    if (scrollPosition >= 15) {
+      // if scroll down hide the navbar
+      setShow(true);
+    } else {
+      // if scroll up show the navbar
+      setShow(false);
+    }
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
+  const handleScroll = () => {
+    const position = window.scrollY;
+    console.log(position)
+
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <nav ref={navRef} className="nav">
+      <nav ref={navRef} className={`nav ${show && "onScroll"}`}>
         <a href="#" className="logo">
           CryptoArtVerse
         </a>
