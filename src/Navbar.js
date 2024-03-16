3942d85442f04eeb5cf946fcd990400628b9287b
akshita -86 <akshitaaeran8776@gmail.com>
2024-03-16 13:56:50 +0530

    glassy navbar on scroll and navbar stick on top

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
 
 const Navbar = () => {
   const navRef = useRef(null);
+  const [color, setColor] = useState(false)
+  const changeColor = () => {
+    if (window.scrollY >=70){
+      setColor(true)
+    }else{
+      setColor(false)
+    }
+  }
+  window.addEventListener('scroll',changeColor)
 
   return (
     <>
-      <nav ref={navRef} className="nav">
+      <nav ref={navRef} className={color ? 'nav nav-bg': 'nav'}>
         <a href="#" className="logo">
           CryptoArtVerse
         </a>
         <div className="navbar" style={{ marginLeft: "-160px" }}>
           <a href="#">Home</a>
           <a href="#lineContainer">About</a>
+          <div class="animation start-home"></div>
           {/* <a href="#">Portfolio</a>
           <a href="#">Service</a>
           <a href="#">Contact</a> */}
