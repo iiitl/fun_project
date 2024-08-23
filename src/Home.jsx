893ef2bd3af465107232a5ae001a37582be5315b
Avishek <avishekmishra56@gmail.com>
2024-08-23 16:05:51 +0530

    enancement of home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -85,7 +85,11 @@ const Home = ({ setProtect }) => {
               you venture into hidden places and uncover the hidden secrets
               remember this the message can be anywhere
             </p>
-            <button id="myBtn">EXPLORE NOW</button>
+            <button id="myBtn">EXPLORE NOW
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg_class">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+              </svg>
+            </button>
           </div>
         </div>
       ) : (
